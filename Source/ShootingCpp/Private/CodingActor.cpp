660e9d59d53aa3773548365e012dd669f1e89721
JY1031 <56060807+JY1031@users.noreply.github.com>
2022-07-06 15:20:35 +0900

    Initial commit

---
+++ Source/ShootingCpp/Private/CodingActor.cpp
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodingActor.h"
+
+// Sets default values
+ACodingActor::ACodingActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	number = 10;
+	UE_LOG(LogTemp, Warning, TEXT("%d"), number);
+
+	fNumber = 3.14f;
+	UE_LOG(LogTemp, Warning, TEXT("%f"), fNumber);
+
+	myName = TEXT("이영훈");
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *myName);
+
+	bGood = false;
+	UE_LOG(LogTemp, Warning, TEXT("%d"), bGood);
+	
+}
+
+// Called when the game starts or when spawned
+void ACodingActor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	
+}
+
+// Called every frame
+void ACodingActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
